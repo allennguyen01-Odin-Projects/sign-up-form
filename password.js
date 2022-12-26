a018f448c4dff95ab3504ff27cdf5431c6b222f4
Allen Nguyen <anhn321@gmail.com>
2022-12-26 13:23:59 -0800

    Add eslint

--- password.js
+++ password.js
@@ -1,19 +1,19 @@
-function password() {
-	const password = document.getElementById("password").value;
-	const passwordConfirm = document.getElementById("password-confirm").value;
-	const submit = document.getElementById("submit-button");
-	const passwordValidation = document.getElementById("password-validation");
+function confirmPassword() {
+  const password = document.getElementById('password').value;
+  const passwordConfirm = document.getElementById('password-confirm').value;
+  const submit = document.getElementById('submit-button');
+  const passwordValidation = document.getElementById('password-validation');
 
-	if (password != passwordConfirm) {
-		submit.disabled = true;
-		submit.classList.add("disabled");
-		passwordValidation.textContent = "❌ Passwords do not match";
-	} else {
-		submit.disabled = false;
-		submit.classList.remove("disabled");
-		passwordValidation.textContent = "✅ Passwords match";
-	}
+  if (password !== passwordConfirm) {
+    submit.disabled = true;
+    submit.classList.add('disabled');
+    passwordValidation.textContent = '❌ Passwords do not match';
+  } else {
+    submit.disabled = false;
+    submit.classList.remove('disabled');
+    passwordValidation.textContent = '✅ Passwords match';
+  }
 }
 
-const passwordConfirm = document.getElementById("password-confirm");
-passwordConfirm.addEventListener("keyup", password);
+const passwordConfirm = document.getElementById('password-confirm');
+passwordConfirm.addEventListener('keyup', confirmPassword);
